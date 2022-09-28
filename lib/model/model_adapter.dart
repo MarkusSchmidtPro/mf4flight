7fcf0843cef4738537dc0d8ad7a63e05ec5c4131
MSPro <Markus@MarkusSchmidt.PRO>
2022-09-28 10:50:58 +0200

    model adapter

--- lib/model/model_adapter.dart
+++ lib/model/model_adapter.dart
@@ -72,12 +72,12 @@ abstract class ModelAdapter<TContext> {
   /// (see [FavoriteViewModelBase]):
   /// ```dart
   /// @override
-  /// Future onViewSaveAsync() async {
-  ///   logger.finest(">onViewSaveAsync()");
+  /// Future onSaveChangesAsync() async {
+  ///   logger.finest(">onSaveChangesAsync()");
   ///   _viewToModel(model);
   ///   await model.saveAsync();
   ///   await serviceProvider<SyncTimer>().triggerSyncRequestAsync();
-  ///   logger.finest("<onViewSaveAsync()");
+  ///   logger.finest("<onSaveChangesAsync()");
   /// }
   /// ```
   Future<void> saveAsync(TContext model) async {
@@ -87,4 +87,77 @@ abstract class ModelAdapter<TContext> {
   Future<void> deleteAsync(TContext model) async {
     assert(true, "$runtimeType does not support deleteAsync for model ${TContext.runtimeType}");
   }
+}
+
+abstract class ModelAdapter2<TContext> {
+  late final Logger logger;
+
+  @protected
+  ModelAdapter2() {
+    logger = new Logger('$runtimeType');
+  }
+
+
+  /// Save the model's data back to its origin.
+  ///
+  /// The _origin_ is defined by the adapter itself, by its _seed_.
+  /// For example a data_model related adapter, may implement saveAsync
+  /// like this (see [FavoriteDatabaseAdapter]):
+  /// ```dart
+  /// class FavoriteDatabaseAdapter extends ModelAdapter<FavoriteModel> {
+  ///   final FavoriteRecord seed;
+  ///   FavoriteDatabaseAdapter(this.seed);
+  ///
+  ///   late final LocalContext _c = serviceProvider<LocalContext>();
+  ///
+  ///   @override
+  ///   Future<void> saveAsync(FavoriteModel model) async {
+  ///     seed.trackChanges(new FavoriteRecord());
+  ///     seed.syncId = model.id;
+  ///     seed.productName = model.productName;
+  ///     ...
+  ///     await _c.favorites.saveChangesAsync(seed);
+  ///     assert(!seed.isTracking());
+  ///   }
+  /// }
+  /// ```
+  /// Use the model's save functionality in the view model
+  /// (see [FavoriteViewModelBase]):
+  /// ```dart
+  /// @override
+  /// Future onSaveChangesAsync() async {
+  ///   logger.finest(">onSaveChangesAsync()");
+  ///   _viewToModel(model);
+  ///   await model.saveAsync();
+  ///   await serviceProvider<SyncTimer>().triggerSyncRequestAsync();
+  ///   logger.finest("<onSaveChangesAsync()");
+  /// }
+  /// ```
+  @protected
+  Future<void> saveAsync(TContext model) async {
+    assert(true, "$runtimeType does not support saveAsync for model ${TContext.runtimeType}");
+  }
+
+  @protected
+  Future<void> deleteAsync(TContext model) async {
+    assert(true, "$runtimeType does not support deleteAsync for model ${TContext.runtimeType}");
+  }
+}
+
+
+mixin AsyncCompletionAdapter<TContext> on ModelAdapter2<TContext>{
+  /// Complete the model by executing an asynchronous load functionality.
+  ///
+  /// Normally, [loadAsync] is called in the
+  /// view model's [ViewModelBase.loadAsync]
+  /// method (see [FavoriteMessageAdapter]):
+  /// ```dart
+  /// @override
+  /// Future<void> loadDataAsync() async {
+  ///   await model.loadAsync();
+  ///   _modelToViewModel(model);
+  ///   notifyListeners();
+  /// }
+  /// ```
+  Future<void> completeAsync(TContext model) ;
 }
